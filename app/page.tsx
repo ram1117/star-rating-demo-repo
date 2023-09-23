9866eea834f10742a6d60bd05af7dfa25aba1719
ram1117 <ramkumark1@outlook.com>
2023-09-23 22:24:29 +0530

    add styling to the star component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import Star from '@/components/star/Star';
 
 export default function Home() {
   return (
-  <div className={styles['main-container']}>Main Container</div>);
+  <div className={`${styles['main-container']}`}>
+    <Star height={48} width={48} stroke='black' fill='gold' style={{border:'2px solid black'}} />
+  </div>);
 }
